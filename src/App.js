680c305925589fdc91387fa1d3358a58d4c74f24
Desmond <desmondfon@yahoo.com>
2022-12-18 07:19:44 +0100

    added a conditional for the game check a setState for score

--- src/App.js
+++ src/App.js
@@ -9,17 +9,75 @@ function App() {
   const [itemPicked, setItemPicked] = useState("");
   const [score, setScore] = useState(0);
 
+  const arr = [
+    "http://localhost:3000/static/media/icon-paper.8b57a6b108c1b8ae82931a39b10f96a2.svg",
+    "http://localhost:3000/static/media/icon-scissors.3b1a5d7eb0b2f1fd3e955865ec04d9ed.svg",
+    "http://localhost:3000/static/media/icon-rock.476e90a9646a87bb3645d2f09f4f7fba.svg",
+  ];
+  let str = arr[Math.floor(Math.random() * arr.length)]
+  // function getMultipleRandom(arr, num) {
+  //   const shuffled = [...arr].sort(() => 0.5 - Math.random());
+
+  //   return shuffled.slice(0, num);
+  // }
+
+  // let str = getMultipleRandom(arr, 1).toString();
+
+  console.log(str);
+  // // console.log(setItemPicked);
+
   const toggleRules = () => setShowRules((value) => !value);
 
+  let el;
+
   const toggleSelectSection = (e) => {
-    const el = e.target;
-    console.log(el);
+    el = e.target;
+    console.log(el.src);
+    console.log(str);
     setItemPicked(el.src);
-    setSelectSection((value) => !value);
+    setSelectSection(true);
+
+    if (
+      (el.src ===
+        "http://localhost:3000/static/media/icon-paper.8b57a6b108c1b8ae82931a39b10f96a2.svg" &&
+        str ===
+          "http://localhost:3000/static/media/icon-rock.476e90a9646a87bb3645d2f09f4f7fba.svg") ||
+      (el.src ===
+        "http://localhost:3000/static/media/icon-rock.476e90a9646a87bb3645d2f09f4f7fba.svg" &&
+        str ===
+          "http://localhost:3000/static/media/icon-scissors.3b1a5d7eb0b2f1fd3e955865ec04d9ed.svg") ||
+      (el.str ===
+        "http://localhost:3000/static/media/icon-scissors.3b1a5d7eb0b2f1fd3e955865ec04d9ed.svg" &&
+        str === "http://localhost:3000/static/media/icon-paper.8b57a6b108c1b8ae82931a39b10f96a2.svg")
+    ) {
+      console.log("you win!");
+      setScore (prevActiveStep => prevActiveStep + 1);
+    } else if (
+      (el.src ===
+        "http://localhost:3000/static/media/icon-paper.8b57a6b108c1b8ae82931a39b10f96a2.svg" &&
+        str ===
+          "http://localhost:3000/static/media/icon-scissors.3b1a5d7eb0b2f1fd3e955865ec04d9ed.svg") ||
+      (el.src ===
+        "http://localhost:3000/static/media/icon-scissors.3b1a5d7eb0b2f1fd3e955865ec04d9ed.svg" &&
+        str ===
+          "http://localhost:3000/static/media/icon-rock.476e90a9646a87bb3645d2f09f4f7fba.svg") ||
+      (el.src ===
+        "http://localhost:3000/static/media/icon-rock.476e90a9646a87bb3645d2f09f4f7fba.svg" &&
+        str === "http://localhost:3000/static/media/icon-paper.8b57a6b108c1b8ae82931a39b10f96a2.svg")
+    ) {
+      if (score > 0){
+        setScore (prevActiveStep => prevActiveStep - 1);
+      }
+      console.log("you lose!");
+    } else {
+      console.log("draw");
+    }
   };
+  // console.log(el)
 
-  console.log(setItemPicked)
-
+  const toggler = () => {
+    setSelectSection(false);
+  }
 
   return (
     <div>
@@ -30,8 +88,11 @@ function App() {
           toggleRules={toggleRules}
           score={score}
           toggleSelectSection={toggleSelectSection}
+          toggler={toggler}
           selectSection={selectSection}
           itemPicked={itemPicked}
+          setItemPicked={el}
+          str={str}
         />
       )}
       {/* <Home /> */}
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,15 +3,34 @@ import Header from "../component/Header";
 import Index from "../component/Index";
 import Select from "../component/Select";
 
-const Home = ({toggleRules, score, selectSection, toggleSelectSection, itemPicked}) => {
-    return ( <div className='bg-gradient-to-b from-radical-gradient-from to-radical-gradient-to h-screen'>
+const Home = ({
+  toggleRules,
+  score,
+  selectSection,
+  toggleSelectSection,
+  itemPicked,
+  setItemPicked,
+  str,
+  toggler
+}) => {
+  return (
+    <div className="bg-gradient-to-b from-radical-gradient-from to-radical-gradient-to h-screen">
+      <Header score={score} />
+      {selectSection ? (
+        <Select
+          itemPicked={itemPicked}
+          setItemPicked={setItemPicked}
+          toggleSelectSection={toggleSelectSection}
+          str={str}
+          toggler={toggler}
+        />
+      ) : (
+        <Index toggleSelectSection={toggleSelectSection} />
+      )}
 
-    <Header score = {score}/>
-    {selectSection? <Select itemPicked = {itemPicked}/> : <Index toggleSelectSection = {toggleSelectSection}/>}
-    
-    
-    <Button toggleRules={toggleRules}/>
-    </div> );
-}
- 
+      <Button toggleRules={toggleRules} />
+    </div>
+  );
+};
+
 export default Home;
